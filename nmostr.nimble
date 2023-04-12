df15286e912497f0794b3dea3e07b1c522d00af1
Gruruya <greytest@disroot.org>
2023-04-12 19:35:18 -0400

    Progress
    * split YAML serialization into seperate file
    * define ssl for cli

--- nmostr.nimble
+++ nmostr.nimble
@@ -10,14 +10,14 @@ skipDirs = @["tests"]
 # Dependencies
 requires "nim >= 1.9.1"
 requires "jsony ^= 1.1"
-requires "adix >= 0.5.2"
-requires "https://github.com/alaviss/union >= 0.1.3"
-requires "https://github.com/Gruruya/nim-secp256k1#less-safe"
 requires "crunchy >= 0.1.8"
+requires "https://github.com/Gruruya/nim-secp256k1#less-safe"
+requires "https://github.com/alaviss/union >= 0.1.3"
 # For client
 requires "cligen ^= 1.6.0"
 requires "yaml ^= 1.1.0"
 requires "whisky ^= 0.1.1"
+requires "adix >= 0.5.2"
 
 taskRequires "test", "https://github.com/disruptek/balls ^= 3.0.0" # Can't do 4.0 because of (incorrect) `union` deps, see https://github.com/alaviss/union/pull/33
 
--- src/nmostr/cli.nim
+++ src/nmostr/cli.nim
@@ -19,8 +19,8 @@
 
 import
   std/[os, strutils, sequtils, sugar, options, streams, random],
-  pkg/yaml/[serialization, presenter, taglib, private/internal], pkg/adix/lptabz, cligen,
-  ../nmostr, ./bech32, ./cli/alias
+  pkg/yaml, pkg/adix/lptabz, cligen,
+  ../nmostr, ./bech32, ./cli/alias, ./cli/lptabz_yaml
 
 from std/strformat import fmt
 from std/terminal import getch
@@ -93,85 +93,6 @@ template randomAccount: (string, Keypair) =
     kp = newKeypair()
     name = generateAlias(kp.seckey.toPublicKey)
   (name, kp)
-
-# YAML serialization
-{.push inline.}
-
-# Taken from `NimYAML/yaml/serialization.nim`, `Table` and `set` replaced with `LPTabz` and `LPSetz`
-proc constructionError(s: YamlStream, mark: Mark, msg: string): ref YamlConstructionError =
-  return newYamlConstructionError(s, mark, msg)
-
-proc safeTagUri(tag: Tag): string {.raises: [].} =
-  try:
-    var uri = $tag
-    # '!' is not allowed inside a tag handle
-    if uri.len > 0 and uri[0] == '!': uri = uri[1..^1]
-    # ',' is not allowed after a tag handle in the suffix because it's a flow
-    # indicator
-    for i in countup(0, uri.len - 1):
-      if uri[i] == ',': uri[i] = ';'
-    return uri
-  except KeyError:
-    internalError("Unexpected KeyError for Tag " & $tag)
-
-proc yamlTag*[K,V:not void,Z,z](T: typedesc[LPTabz[K, V, Z, z]]): Tag {.raises: [].} =
-  return nimTag("tables:Table(" & safeTagUri(yamlTag(K)) & ';' & safeTagUri(yamlTag(V)) & ")")
-
-proc constructObject*[K,V:not void,Z,z](s: var YamlStream, c: ConstructionContext,
-                            result: var LPTabz[K, V, Z, z]) {.raises: [YamlConstructionError, YamlStreamError].} =
-  let event = s.next()
-  if event.kind != yamlStartMap:
-    return # Skip empty `field:` Could be made more specific/correct possibly
-  result = initLPTabz[K, V, Z, z]()
-  while s.peek.kind != yamlEndMap:
-    var
-      key: K
-      value: V
-    constructChild(s, c, key)
-    constructChild(s, c, value)
-    if result.contains(key):
-      raise s.constructionError(event.startPos, "Duplicate table key!")
-    try: result[key] = value
-    except IOError: raise s.constructionError(event.startPos, "IOError when attempting to include into LPTabz")
-  discard s.next()
-
-proc representObject*[K,V:not void,Z,z](value: LPTabz[K, V, Z, z], ts: TagStyle, c: SerializationContext, tag: Tag) =
-  let childTagStyle = if ts == tsRootOnly: tsNone else: ts
-  c.put(startMapEvent(tag = tag))
-  if value.len != 0:
-    for key, value in value.pairs:
-      representChild(key, childTagStyle, c)
-      representChild(value, childTagStyle, c)
-  else:
-    # should put {}
-    discard
-  c.put(endMapEvent())
-
-proc yamlTag*[K,Z,z](T: typedesc[LPSetz[K, Z, z]]): Tag {.raises: [].} =
-  return nimTag("system:set(" & safeTagUri(yamlTag(K)) & ')')
-
-proc constructObject*[K,Z,z](s: var YamlStream, c: ConstructionContext, result: var LPSetz[K,Z,z])
-    {.raises: [YamlConstructionError, YamlStreamError].} =
-  let event = s.next()
-  if event.kind != yamlStartSeq:
-    raise s.constructionError(event.startPos, "Expected sequence start")
-  result = initLPSetz[K,Z,z]()
-  while s.peek().kind != yamlEndSeq:
-    var item: K
-    constructChild(s, c, item)
-    try: result.incl(item)
-    except IOError: raise s.constructionError(event.startPos, "IOError when attempting to include into LPSetz") # should use proper mark rather than the start (event.startPos)
-  discard s.next()
-
-proc representObject*[K,Z,z](value: LPSetz[K,Z,z], ts: TagStyle, c: SerializationContext, tag: Tag) =
-  let childTagStyle = if ts == tsRootOnly: tsNone else: ts
-  c.put(startSeqEvent(tag = tag))
-  for item in value:
-    representChild(item, childTagStyle, c)
-  c.put(endSeqEvent())
-
-{.pop inline.}
-# End of YAML serialization
 
 template setAccount(config: Config, name: string, kp: KeyPair, echo: bool): string =
   if not echo:
