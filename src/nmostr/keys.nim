930ee95aaabb87c3b05637dc236c02372c6c2bef
Gruruya <gruruya.chi4c@slmails.com>
2023-05-13 16:17:22 -0400

    Improve new `keys.nim` module

--- src/nmostr/keys.nim
+++ src/nmostr/keys.nim
@@ -21,52 +21,56 @@ import pkg/secp256k1, pkg/secp256k1/abi
 from pkg/stew/byteutils import toHex
 export secp256k1 except default
 
-type SecretKey* = SkSecretKey
+{.push raises: [], inline.}
 
-type PublicKey* = object
-  ## Representation of public key that only reveals the x-coordinate.
-  ## Modified from `secp256k1` to not have `{.requiresInit.}`
-  data: secp256k1_xonly_pubkey
+type
+  SecretKey* = SkSecretKey
 
+  PublicKey* = object
+    ## Representation of public key that only reveals the x-coordinate.
+    ## Modified from `secp256k1` to not have `{.requiresInit.}`
+    data: secp256k1_xonly_pubkey
+
+  SchnorrSignature* = object
+    ## Representation of a Schnorr signature.
+    ## Modified from `secp256k1` to not have `{.requiresInit.}`
+    data: array[SkRawSchnorrSignatureSize, byte]
+
+# Equivalent to `secp256k1` types minues {.requireInit.}, so allow casting for interop
 converter toPublicKey*(pubkey: SkXOnlyPublicKey): PublicKey = cast[PublicKey](pubkey)
 converter toSkXOnlyPublicKey*(pubkey: PublicKey): SkXOnlyPublicKey = cast[SkXOnlyPublicKey](pubkey)
-
-func fromHex*(T: type PublicKey, data: string): SkResult[T] =
-  cast[SkResult[PublicKey]](SkXOnlyPublicKey.fromHex(data))
-
-func fromRaw*(T: type PublicKey, data: openArray[byte]): SkResult[T] {.inline.} =
-  if likely data.len == 32: cast[SkResult[PublicKey]](SkXOnlyPublicKey.fromRaw(data))
-  elif data.len == 33: cast[SkResult[PublicKey]](SkXOnlyPublicKey.fromRaw(data))
-  else: raise newException(ValueError, "Raw x-only public key must be 32 or 33 bytes")
-
-func toRaw*(pubkey: PublicKey): array[SkRawXOnlyPublicKeySize, byte] =
-  ## Wrapper to check if pubkey is uninitialized
-  if pubkey == default(PublicKey): return
-  else: secp256k1.toRaw(pubkey)
-
-func toHex*(pubkey: PublicKey): string =
-  toHex(toRaw(pubkey))
-
-type SchnorrSignature* = object
-  ## Representation of a Schnorr signature.
-  ## Modified from `secp256k1` to not have `{.requiresInit.}`
-  data: array[SkRawSchnorrSignatureSize, byte]
-
 converter toSchnorrSignature*(sig: SkSchnorrSignature): SchnorrSignature = cast[SchnorrSignature](sig)
 converter toSkSchnorrSignature*(sig: SchnorrSignature): SkSchnorrSignature = cast[SkSchnorrSignature](sig)
 
-func fromHex*(T: type SchnorrSignature, data: string): SkResult[T] =
-  cast[SkResult[SchnorrSignature]](SkSchnorrSignature.fromHex(data))
+func fromRaw*(T: type PublicKey, data: openArray[byte]): SkResult[T] =
+  ## Additionally accepts 33 byte compressed public keys, should upstream this
+  if likely data.len == 32: cast[SkResult[PublicKey]](SkXOnlyPublicKey.fromRaw(data))
+  elif data.len == 33: cast[SkResult[PublicKey]](SkXOnlyPublicKey.fromRaw(data[1..^1]))
+  else: err(static("secp: x-only public key must be 32 or 33 bytes"))
+
+func fromHex*(T: type PublicKey, data: string): SkResult[T] =
+  T.fromRaw(? seq[byte].fromHex(data))
 
 func fromRaw*(T: type SchnorrSignature, data: openArray[byte]): SkResult[T] =
   cast[SkResult[SchnorrSignature]](SkSchnorrSignature.fromRaw(data))
 
+func fromHex*(T: type SchnorrSignature, data: string): SkResult[T] =
+  T.fromRaw(? seq[byte].fromHex(data))
+
+func toRaw*(pubkey: PublicKey): array[SkRawXOnlyPublicKeySize, byte] =
+  ## Wrapper to check if pubkey is uninitialized
+  if pubkey == default(PublicKey): return
+  secp256k1.toRaw(pubkey)
+
+func toHex*(pubkey: PublicKey): string =
+  toHex(toRaw(pubkey))
+
 func verify*(sig: SchnorrSignature, msg: SkMessage, pubkey: PublicKey): bool =
   ## Wrapper to check if pubkey is uninitialized
   if pubkey == default(typeof pubkey): return false
-  else: secp256k1.verify(sig, msg, pubkey)
+  secp256k1.verify(sig, msg, pubkey)
 
 func verify*(sig: SchnorrSignature, msg: openArray[byte], pubkey: PublicKey): bool =
   ## Wrapper to check if pubkey is uninitialized
   if pubkey == default(typeof pubkey): return false
-  else: secp256k1.verify(sig, msg, pubkey)
+  secp256k1.verify(sig, msg, pubkey)
