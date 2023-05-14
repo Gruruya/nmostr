412ce145034c78dd5bce2104e5010b3cdf4f55c8
Gruruya <gruruya.chi4c@slmails.com>
2023-05-14 18:17:08 -0400

    Type `uint5` correctly in bech32 rather than using runtime checks

--- src/nmostr/bech32.nim
+++ src/nmostr/bech32.nim
@@ -26,7 +26,7 @@ import ./events
 
 {.push raises: [].}
 
-type int5* = int
+type uint5* = range[0.uint32..31.uint32]
 
 type InvalidBech32Error* = object of ValueError
 
@@ -40,65 +40,63 @@ func fromRaw(T: type PublicKey, data: openArray[byte]): SkResult[T] {.inline, ra
   else: raise newException(InvalidBech32Error, "Raw x-only public key must be 32 or 33 bytes")
 
 const CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
-const CHARSET_MAP = CHARSET.mapIt((it, CHARSET.find(it).int5)).toTable()
+const CHARSET_MAP = CHARSET.mapIt((it, CHARSET.find(it).uint5)).toTable()
 
-func toWords*(data: openArray[byte]): seq[int5] =
-  ## int8 → int5 conversion
+func toWords*(data: openArray[byte]): seq[uint5] =
+  ## uint8 → uint5 conversion
   var acc, bits = 0
   const maxV = (1 shl 5) - 1
   let outputLen = (data.len * 8 + 4) div 5
-  result = newSeq[int5](outputLen)
+  result = newSeq[uint5](outputLen)
   var idx = 0
   for value in data:
     acc = (acc shl 8) or ord(value)
     bits += 8
     while bits >= 5:
       bits -= 5
-      result[idx] = int5((acc shr bits) and maxV)
+      result[idx] = uint5((acc shr bits) and maxV)
       inc(idx)
-  result[idx] = int5((acc shl (5 - bits)) and maxV)
+  result[idx] = uint5((acc shl (5 - bits)) and maxV)
 
-func fromWords*(data: openArray[int5]): seq[byte] {.raises: [InvalidBech32Error].} =
-  ## int5 → int8 conversion
-  var acc = 0.int5
-  var bits = 0.int8
+func fromWords*(data: openArray[uint5]): seq[byte] =
+  ## uint5 → uint8 conversion
+  var acc = 0.uint5
+  var bits = 0.uint8
   const maxV = (1 shl 8) - 1
   let outputLen = (data.len * 5) div 8
   result = newSeq[byte](outputLen)
   var idx = 0
   for value in data:
-    acc = (acc shl 5.int5) or value
+    acc = (acc shl 5.uint5) or value
     bits += 5
     while bits >= 8:
       bits -= 8
       result[idx] = ((acc shr bits) and maxV).byte
       inc(idx)
-  if unlikely bits >= 5: error "Excess padding"
-  elif unlikely ((acc shl (8 - bits)) and maxV) != 0: error "Non-zero padding"
 
-func polymod(values: openArray[int5]): int5 =
-  const generator = [int5 0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
+func polymod(values: openArray[uint5]): int =
+  const generator = [int 0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
   result = 1
   for value in values:
     let top = result shr 25
-    result = (result and 0x1ffffff) shl 5 xor value
+    result = (result and 0x1ffffff) shl 5 xor value.int
     for i in 0 ..< 5:
       result = result xor (if (top shr i and 1) == 1: generator[i] else: 0)
 
-func hrpExpand(hrp: string): seq[int5] =
-  result = newSeq[int5](hrp.len * 2 + 1)
+func hrpExpand(hrp: string): seq[uint5] =
+  result = newSeq[uint5](hrp.len * 2 + 1)
   for i, c in hrp:
-    result[i] = int5(c) shr 5
-    result[i + hrp.len + 1] = int5(c) and 31
+    result[i] = uint5(ord(c) shr 5)
+    result[i + hrp.len + 1] = uint5(ord(c) and 31)
 
 func encode*(hrp: string, witprog: openArray[byte]): string =
   ## Encode into a bech32 address
-  func checksum(hrp: string, data: seq[int5]): seq[int5] {.inline.} =
+  func checksum(hrp: string, data: seq[uint5]): seq[uint5] {.inline.} =
     let values = hrpExpand(hrp) & data
-    let polymod = polymod(values & @[int5 0, 0, 0, 0, 0, 0]) xor 1
-    result = newSeqOfCap[int5](5)
+    let polymod = polymod(values & @[uint5 0, 0, 0, 0, 0, 0]) xor 1
+    result = newSeqOfCap[uint5](5)
     for i in 0 .. 5:
-      result.add (polymod shr (5 * (5 - i))) and 31
+      result.add uint5((polymod shr (5 * (5 - i))) and 31)
   
   let data = toWords(witprog)
   let combined = data & checksum(hrp, data)
@@ -109,10 +107,10 @@ func encode*(hrp: string, witprog: openArray[byte]): string =
 func encode*(hrp, witprog: string): string {.inline.} =
   encode(hrp, witprog.toBytes)
   
-proc verifyChecksum*(hrp: string, data: seq[int5]): bool {.inline.} =
+proc verifyChecksum*(hrp: string, data: seq[uint5]): bool {.inline.} =
   polymod(hrpExpand(hrp) & data) == 1
 
-template decodeImpl(bech32: string): tuple[hrp: string, data: seq[int5]] =
+template decodeImpl(bech32: string): tuple[hrp: string, data: seq[uint5]] =
   bech32 = bech32.toLower()
   let pos = bech32.rfind('1')
   if unlikely pos < 1 or unlikely pos + 7 > bech32.len: # or len(bech32) > 90:
@@ -181,17 +179,17 @@ func fromUInt32(data: uint32): array[4, byte] {.inline.} =
   for i in 0..<4:
     result[3 - i] = byte((data shr (i * 8)) and 0xFF)
 
-template parseData(address: openArray[byte], i: var int): tuple[kind: int8, data: seq[byte]] =
-  if i + 1 >= address.len: break
-  let (kind, length) = (cast[int8](address[i]), cast[int8](address[i + 1]))
+template parseData(address: openArray[byte], i: var uint32): tuple[kind: uint8, data: seq[byte]] =
+  if i + 1 >= address.len.uint32: break
+  let (kind, length) = (cast[uint8](address[i]), cast[uint8](address[i + 1]))
   i += 2
-  if unlikely i + length - 1 > address.len: error "End of value " & $(i + length - 1) & " exceeds bech32 address length " & $address.len
+  if unlikely i + length - 1 > address.len.uint32: error "End of value " & $(i + length - 1) & " exceeds bech32 address length " & $address.len
   let data = address[i..<i + length]
   i += length
   (kind, data)
 
 func fromRaw*(T: type NProfile, address: openArray[byte]): T {.raises: [InvalidBech32Error].} =
-  var i = 0
+  var i = 0.uint32
   while true:
     let (kind, data) = parseData(address, i)
     case kind:
@@ -205,7 +203,7 @@ func fromRaw*(T: type NProfile, address: openArray[byte]): T {.raises: [InvalidB
       discard
 
 func fromRaw*(T: type NEvent, address: openArray[byte]): T {.raises: [InvalidBech32Error].} =
-  var i = 0
+  var i = 0.uint32
   while true:
     let (kind, data) = parseData(address, i)
     case kind:
@@ -225,7 +223,7 @@ func fromRaw*(T: type NEvent, address: openArray[byte]): T {.raises: [InvalidBec
       discard
 
 func fromRaw*(T: type NAddr, address: openArray[byte]): T {.raises: [InvalidBech32Error].} =
-  var i = 0
+  var i = 0.uint32
   while true:
     let (kind, data) = parseData(address, i)
     case kind:
@@ -243,7 +241,7 @@ func fromRaw*(T: type NAddr, address: openArray[byte]): T {.raises: [InvalidBech
       discard
 
 func fromRaw*(T: type NRelay, address: openArray[byte]): T {.raises: [InvalidBech32Error].} =
-  var i = 0
+  var i = 0.uint32
   while true:
     let (kind, data) = parseData(address, i)
     if likely kind == 0:
