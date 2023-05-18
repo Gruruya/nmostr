58197ed38218649837894e8ddbc55972b225e181
Gruruya <gruruya.chi4c@slmails.com>
2023-05-18 14:12:47 -0400

    Add test case for 4d910a5

--- tests/test.nim
+++ tests/test.nim
@@ -78,6 +78,22 @@ suite "events":
     check a.toJson.fromJson(Filter) == b.toJson.fromJson(Filter) # stores only seconds, nanoseconds are discarded
     check a.toJson != c.toJson
 
+  block example_event_is_valid:
+    check """
+{
+    "id": "4376c65d2f232afbe9b882a35baa4f6fe8667c4e684749af565f981833ed6a65",
+    "pubkey": "6e468422dfb74a5738702a8823b9b28168abab8655faacb6853cd0ee15deee93",
+    "created_at": 1673347337,
+    "kind": 1,
+    "tags": [
+        ["e", "3da979448d9ba263864c4d6f14984c423a3838364ec255f03c7904b1ae77f206"],
+        ["p", "bf2376e17ba4ec269d10fcc996a4746b451152be9031fa48e74553dde5526bce"]
+    ],
+    "content": "Walled gardens became prisons, and nostr is the first step towards tearing down the prison walls.",
+    "sig": "908a15e46fb4d8675bab026fc230a0e3542bfade63da02d542fb78b2a8513fcd0092619a2c8c1221e581946e0191f2af505dfdf8657a414dbca329186f009262"
+}
+""".fromJson(Event).verify
+
 suite "messages":
   block serializing_and_parsing:
     check CMEvent(event: Event()) == ("[\"EVENT\"," & Event().toJson & "]").fromMessage
