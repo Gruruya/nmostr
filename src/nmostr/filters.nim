43c5b139bac5260b5a394a48f56ea51b2d642051
Gruruya <gruruya.chi4c@slmails.com>
2023-08-02 13:03:08 -0400

    Improve filter matching

--- src/nmostr/filters.nim
+++ src/nmostr/filters.nim
@@ -17,7 +17,7 @@
 
 ## Nostr filters and utilities for using them.
 
-import std/[times, strutils, sequtils, sugar, macros]
+import std/[times, strutils, sequtils, macros]
 import std/json except JsonError
 import pkg/jsony
 import ./events
@@ -36,23 +36,31 @@ type Filter* = object
   search*: string         ## A query in a human-readable form (NIP-50)
   other*: seq[(string, JsonNode)] ## Catch-all for unknown fields
 
-func stripGeneric(tag: string): string {.inline.} =
-  if likely tag.len > 1 and likely tag[0] == '#': tag[1..^1]
-  else: tag
+func isGenericTag(key: string): bool {.inline.} =
+  ## Checks if a filter's key is a generic tag query according to NIP-12
+  key.len == 2 and key[0] == '#' and likely key[1] in Letters
+
+func tagsMatch(filterTags, eventTags: seq[seq[string]]): bool =
+  func genericTagsMatch(ftag, etag: seq[string]): bool =
+    if etag[0].len == 1 and ftag[0][1] == etag[0][0]:
+      if ftag.len == 1 or ftag.len == 2 and ftag[1] == "": return true
+      for value in ftag[1..^1]:
+        if etag[1] == value: return true
+
+  for tag in filterTags:
+    if tag[0].isGenericTag:
+      if anyIt(eventTags, genericTagsMatch(tag, it)): return true
 
 func matches*(event: Event, filter: Filter): bool =
   ## Determine if `event` matches `filter`.
-  filter.since < event.created_at and
-  filter.until > event.created_at and
-  (filter.kinds == @[] or anyIt(filter.kinds, event.kind == it)) and
-  (filter.ids == @[] or anyIt(filter.ids, event.id.`$`.startsWith it)) and
-  (filter.authors == @[] or anyIt(filter.authors, event.pubkey.toHex.startsWith it)) and
-  (filter.tags == @[] or any(filter.tags, ftags => likely ftags.len > 1 and any(event.tags, etags => likely etags.len > 1 and etags[0] == ftags[0].stripGeneric and etags[1] == ftags[1])))
+  filter.since <= event.created_at and
+  filter.until >= event.created_at and
+  (filter.kinds.len == 0 or anyIt(filter.kinds, event.kind == it)) and
+  (filter.ids.len == 0 or (let id=event.id.toHex; anyIt(filter.ids, id.startsWith it))) and
+  (filter.authors.len == 0 or (let pubkey=event.pubkey.toHex; anyIt(filter.authors, pubkey.startsWith it))) and
+  (filter.tags.len == 0 or tagsMatch(filter.tags, event.tags))
 
 # JSON interop
-macro fieldAccess(o: object, s: string): untyped =
-  newDotExpr(o, newIdentNode(s.strVal))
-
 proc parseHook*(s: string, i: var int, v: var Filter) {.raises: [JsonError, ValueError].} =
   ## Parse filters exactly the same as a normal object, but add each field starting with # as an entry in `tags`.
 
@@ -68,7 +76,7 @@ proc parseHook*(s: string, i: var int, v: var Filter) {.raises: [JsonError, Valu
   when compiles(newHook(v)):
     newHook(v)
   # Set `until`'s default, `jsony` should implement a generic version of this, such as v = default(typeof v)
-  v.until = Filter().until
+  v.until = static: default(Filter).until
   while i < s.len:
     eatSpace(s, i)
     if i < s.len and s[i] == '}':
@@ -81,15 +89,13 @@ proc parseHook*(s: string, i: var int, v: var Filter) {.raises: [JsonError, Valu
     var parsed = false
     for k, v in v.fieldPairs:
       if k == key:
-        var v2: type(v)
-        parseHook(s, i, v2)
-        v = v2
+        parseHook(s, i, v)
         parsed = true
         break
     if not parsed:
       eatSpace(s, i)
       if likely i < s.len:
-        if s[i] == '[' and key.len == 2 and key[0] == '#' and key[1] in {'A'..'z'}:
+        if s[i] == '[' and key.isGenericTag:
           var j: seq[string]
           parseHook(s, i, j)
           v.tags.add key & j
@@ -140,9 +146,17 @@ proc dumpHook*(s: var string, v: Filter) {.raises: [JsonError, ValueError].} =
         else:
           skipValue(s, i)
     else:
-      if e != default(Filter).fieldAccess(k) and (when k == "until": e.toUnix != high(int64) else: true): # Complex way of checking if the field is empty
+      macro fieldAccess(o: object, s: string): untyped {.used.} =
+        newDotExpr(o, newIdentNode(s.strVal))
+
+      if (when k == "until": e.toUnix != high(int64) else: e != default(Filter).fieldAccess(k)): # Complex way of checking if the field is empty
         if i > 1: s.add ','
         s.dumpKey(k)
         s.dumpHook(e)
         inc i
   s.add '}'
+
+when isMainModule:
+  let e = note(newKeypair(), "test", tags = @[@["e", "not empty"]])
+  var f = Filter(ids: @[e.id.toHex], authors: @[e.pubkey.toHex], kinds: @[1], tags: @[@["#e", "not in event", "not empty"]], search: "", other: @[])
+  doAssert e.matches(f)
